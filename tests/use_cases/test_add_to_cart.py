88871dc63c0a3546eceb8151dfc8b678c74ef861
Olga Letkova <olja.let52@gmail.com>
2024-05-09 15:20:03 +0300

    Test add to cart refactoring

--- tests/use_cases/test_add_to_cart.py
+++ tests/use_cases/test_add_to_cart.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -10,6 +11,13 @@ def test_add_item_to_cart(browser, url):
     browser.get(url)
     add_item_button_list = browser.find_elements(By.CSS_SELECTOR, "button[formaction$='cart.add']")
     a = random.randint(0, len(add_item_button_list) - 1)
-    wait = WebDriverWait(browser, 1)
+    wait = WebDriverWait(browser, 2)
     browser.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.CONTROL + Keys.END)
-    wait.until(EC.element_to_be_clickable((By.XPATH, f"//button[@title='Add to Cart'][{a}]"))).click()
+    time.sleep(2)
+    browser.find_elements(By.CSS_SELECTOR, "button[formaction$='cart.add']")[a].click()
+    browser.find_element(By.CSS_SELECTOR, 'html').send_keys(Keys.PAGE_UP)
+    wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "div[class='alert alert-success alert-dismissible']")))
+    wait.until(EC.element_to_be_clickable((By.ID, "header-cart"))).click()
+    item_quantity = browser.find_element(By.CSS_SELECTOR, "td[class='text-end']").text
+    assert item_quantity == "x 1", "Количество товара не равно 1"
